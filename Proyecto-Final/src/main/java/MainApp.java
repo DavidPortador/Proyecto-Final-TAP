ed76d9c69cc44c8dd9ac7ca8bbd4640c3e2bdd9e
DavidPortador <david.raw172@gmail.com>
2021-05-19 21:10:53 -0500

    main v1

---
+++ Proyecto-Final/src/main/java/MainApp.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setTitle("Proyecto Final Papus");
+        Parent v_root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Scene v_sc = new Scene(v_root);
+        primaryStage.setResizable(false);
+        primaryStage.setScene(v_sc);
+        primaryStage.show();
+    }
+}

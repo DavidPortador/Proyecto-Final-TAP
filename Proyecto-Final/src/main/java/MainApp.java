19b3b89f8fc4dc3fbf788747bc143e922dcb04f2
DavidPortador <david.raw172@gmail.com>
2021-05-22 22:16:19 -0500

    Avance de disenio

--- Proyecto-Final/src/main/java/MainApp.java
+++ Proyecto-Final/src/main/java/MainApp.java
@@ -3,20 +3,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import org.kordamp.bootstrapfx.BootstrapFX;
 import java.io.IOException;
-
 public class MainApp extends Application {
-
     public static void main(String[] args) {
         launch(args);
     }
-
     @Override
     public void start(Stage primaryStage) throws IOException {
         primaryStage.setTitle("Proyecto Final Papus");
         Parent v_root = FXMLLoader.load(getClass().getResource("main.fxml"));
         Scene v_sc = new Scene(v_root);
+        v_sc.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
         primaryStage.setResizable(false);
         primaryStage.setScene(v_sc);
         primaryStage.show();
--- Proyecto-Final/src/main/java/controllers/Main.java
+++ Proyecto-Final/src/main/java/controllers/Main.java
@@ -1,4 +1,19 @@
 package controllers;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import java.net.URL;
+import java.util.ResourceBundle;
+public class Main implements Initializable {
+    @FXML
+    RadioButton rdp1si, rdp1no;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ToggleGroup group = new ToggleGroup();
 
-public class Main {
+        rdp1si.setToggleGroup(group);
+
+        rdp1no.setToggleGroup(group);
+    }
 }

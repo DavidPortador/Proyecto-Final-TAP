626f1aab4ba3d98b0c959559578ba15fe00f35a3
DavidPortador <david.raw172@gmail.com>
2021-06-21 11:03:40 -0500

    Correccion de los estilos de las interfaces

--- Proyecto-Final/src/main/java/Database/UserDAO.java
+++ Proyecto-Final/src/main/java/Database/UserDAO.java
@@ -1,11 +1,9 @@
 package Database;
 import javafx.scene.control.Alert;
-
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 public class UserDAO {
     Connection conn;
     public UserDAO (Connection conn) {
@@ -14,7 +12,7 @@ public class UserDAO {
     public String getUsuario(String user, String pass) throws SQLException {
         String consulta, usuario = null;
         consulta = "select A.nombre from Asignacion A inner join Usuario U on A.noUsuario = U.noUsuario " +
-                "where (U.usuario = '"+user+"' or U.correo = '"+user+"') and U.contrase = '"+pass+"'";
+                "where (U.usuario = '"+user+"' or U.correo = '"+user+"') and U.contra = '"+pass+"'";
         Statement st = conn.createStatement();
         ResultSet rs = st.executeQuery(consulta);
         if (rs != null) {
